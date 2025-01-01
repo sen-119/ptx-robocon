f225fcd31d0218a30ac25391f4ad9c2e51a2565f
sen1192012 <sen.take119@gmail.com>
2025-01-01 17:34:44 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+namespace comment {
+    //% blockId=show_strings block="回転サーボ %v"
+    export function noaction(): void {
+    servos.P1.run(-50)
+    servos.P2.run(50)
+    }
+}
+basic.forever(function () {
+    comment.noaction()
+})
