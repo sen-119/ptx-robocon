3f6c6716f53cbb133b6b50b54104db3c83f092e3
sen <sen.take119@gmail.com>
2025-01-14 19:26:21 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,16 @@
 namespace comment {
     //% blockId=show_strings block="回転サーボ %v"
     let speed: number
-    export function noaction(): void {
+    export function noaction1(): void {
     let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
     servos.P1.run(speed - speed * 2)
-    servos.P2.run(speed)
+    servos.P2.run(speed * 2)
     strip.showRainbow(1, 360)
     }
-    //% blockId=show_strings block="コメント %v"
+    //% blockId=show_strings block="速度"
     export function noaction2(speed: string): void {
     }
 }
-
 basic.forever(function () {
 	
 })
